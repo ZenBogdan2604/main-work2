af078b3aae44e2077d56514f32621a143d9eceb4
ZenBogdan2604 <zenbog2008@gmail.com>
2024-12-17 09:05:50 +0300

    add oop to some js files

--- js/comment.js
+++ js/comment.js
@@ -1,20 +1,29 @@
-const commentContent = document.querySelector('.comment__content');
+class CommentLoader {
+    constructor(commentContentSelector) {
+        this.commentContent = document.querySelector(commentContentSelector);
+    }
 
-fetch('https://672dfd95fd89797156449049.mockapi.io/comment')
-.then(response => response.json())
-.then(comments => {
-    comments.forEach(comment => {
-        commentContent.innerHTML += `
-        <div class="comment__item">
-            <div class="comment__avatar-block">
-                <img class="comment__img" src='${comment.img}'>
-                <p class="comment__name">${comment.name}</p>
-            </div>
-            <p class="comment__text">${comment.text}</p>
-        </div>
-    `;
-    });
-})
-.catch(error => {
-    commentContent.innerHTML = '<p>Не удалось загрузить комментарии.</p>';
-});
+    async fetchComments() {
+        const response = await fetch('https://672dfd95fd89797156449049.mockapi.io/comment');
+        const comments = await response.json();
+        this.renderComments(comments);
+    }
+
+    renderComments(comments) {
+        this.commentContent.innerHTML = '';
+        comments.forEach(comment => {
+            this.commentContent.innerHTML += `
+                <div class="comment__item">
+                    <div class="comment__avatar-block">
+                        <img class="comment__img" src="${comment.img}">
+                        <p class="comment__name">${comment.name}</p>
+                    </div>
+                    <p class="comment__text">${comment.text}</p>
+                </div>
+            `;
+        });
+    }
+}
+
+const commentLoader = new CommentLoader('.comment__content');
+commentLoader.fetchComments();
--- js/find.js
+++ js/find.js
@@ -1,52 +1,66 @@
-async function getResponse() {
-  const response = await fetch('https://672dfd95fd89797156449049.mockapi.io/Monument');
-  let content = await response.json();
-  const list = document.querySelector('.find__block-card');
-  const searchInput = document.getElementById('searchInfo');
-  const cardFinder = document.getElementById('card-finder');
-    function displayCards(items) {
-      list.innerHTML = ''; 
-      items.forEach(item => {
-        const cardSection = document.createElement('section');
-        cardSection.id = `card-${item.id}`;
-        cardSection.classList.add('card__pag');
-        cardSection.innerHTML = `
-          <div class="card__card">
-            <div class="card__card-block">
-              <img class="card__card-pic" src="${item.img}" alt="${item.title}">
-              <p class="card__card-txt-pic">${item.title}</p>
-            </div>
-            <p class="card__card-txt">${item.text}</p>
-            <p class="card__card-add">${item.addres}</p>
-          </div>
-        `;
-        cardSection.addEventListener('click', () => {
-          const urlParams = new URLSearchParams();
-          urlParams.append('title', item.title);
-          urlParams.append('img', item.img);
-          urlParams.append('details', item.details);
-          urlParams.append('map', item.map);
-          urlParams.append('addres', item.addres);
-  
-          window.location.href = `info.html?${urlParams}`;
-        });
-  
-        list.append(cardSection);
-      });
-    }
-  function filterCards() {
-      const searchQuery = searchInput.value.trim().toLowerCase();
-      const selectedCategory = cardFinder.value;
-      const filteredContent = content.filter(item => {
-          const categoryMatch = !selectedCategory || (item.filter && item.filter.includes(selectedCategory));
-          const titleMatch = item.title.toLowerCase().includes(searchQuery);
-          return categoryMatch && titleMatch; 
-      });
-      displayCards(filteredContent);
+class CardFinderApp {
+  constructor(listSelector, searchInputId, cardFinderId) {
+    this.list = document.querySelector(listSelector);
+    this.searchInput = document.getElementById(searchInputId);
+    this.cardFinder = document.getElementById(cardFinderId);
+    this.content = [];
   }
-  searchInput.addEventListener('input', filterCards);
-  cardFinder.addEventListener('change', filterCards);
-  displayCards(content);
+
+  async fetchData() {
+    const response = await fetch('https://672dfd95fd89797156449049.mockapi.io/Monument');
+    this.content = await response.json();
+    this.displayCards(this.content);
+  }
+
+  displayCards(items) {
+    this.list.innerHTML = '';
+    items.forEach(item => {
+      const cardSection = document.createElement('section');
+      cardSection.id = `card-${item.id}`;
+      cardSection.classList.add('card__pag');
+      cardSection.innerHTML = `
+        <div class="card__card">
+          <div class="card__card-block">
+            <img class="card__card-pic" src="${item.img}" alt="${item.title}">
+            <p class="card__card-txt-pic">${item.title}</p>
+          </div>
+          <p class="card__card-txt">${item.text}</p>
+          <p class="card__card-add">${item.addres}</p>
+        </div>
+      `;
+      cardSection.addEventListener('click', () => this.handleCardClick(item));
+      this.list.append(cardSection);
+    });
+  }
+
+  handleCardClick(item) {
+    const params = {
+      title: item.title,
+      img: item.img,
+      details: item.details,
+      map: item.map,
+      addres: item.addres
+    };
+    const urlParams = new URLSearchParams(params);
+    window.location.href = `info.html?${urlParams}`;
+  }
+
+  filterCards() {
+    const searchQuery = this.searchInput.value.trim().toLowerCase();
+    const selectedCategory = this.cardFinder.value;
+    const filteredContent = this.content.filter(item =>
+      (!selectedCategory || (item.filter && item.filter.includes(selectedCategory))) &&
+      item.title.toLowerCase().includes(searchQuery)
+    );
+    this.displayCards(filteredContent);
+  }
+
+  init() {
+    this.fetchData();
+    this.searchInput.addEventListener('input', () => this.filterCards());
+    this.cardFinder.addEventListener('change', () => this.filterCards());
+  }
 }
 
-getResponse();
+const cardFinderApp = new CardFinderApp('.find__block-card', 'searchInfo', 'card-finder');
+cardFinderApp.init();
